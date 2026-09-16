627c69bbceaf436de99577c67f94003918a2c2f9
Júlia de Bona Jankus <juliabonaj@gmail.com>
2026-09-16 10:58:11 -0300

    test: aplica 10% de juros entre 13x e 18x

--- src/exercicios/TDD/parcelamento.test.ts
+++ src/exercicios/TDD/parcelamento.test.ts
@@ -27,5 +27,11 @@ describe('calcularParcelamento', () => {
       expect(calcularParcelamento(1000, 9)).toEqual({ valorParcela: 120, totalParcelas: 9 })
       expect(calcularParcelamento(1000, 12)).toEqual({ valorParcela: 90, totalParcelas: 12 })
     })
+
+    it('aplica 10% sobre o total quando for de 13x a 18x', () => {
+      // 1000 + 10% = 1100
+      expect(calcularParcelamento(1000, 13)).toEqual({ valorParcela: 84.62, totalParcelas: 13 })
+      expect(calcularParcelamento(1000, 18)).toEqual({ valorParcela: 61.11, totalParcelas: 18 })
+    })
   })
 })
