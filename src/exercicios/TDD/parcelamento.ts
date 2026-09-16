503c1953b584a8a930c447f7777f43eec53f8f32
Júlia de Bona Jankus <juliabonaj@gmail.com>
2026-09-16 10:55:45 -0300

    feat: implementa faixa de juros de 5% (5x-8x)

--- src/exercicios/TDD/parcelamento.ts
+++ src/exercicios/TDD/parcelamento.ts
@@ -7,7 +7,13 @@ export function calcularParcelamento(
   valorCompra: number,
   numeroParcelas: number,
 ): ResultadoParcelamento {
-  const valorParcela = valorCompra / numeroParcelas
+  const total = valorCompra * (1 + obterJuros(numeroParcelas))
+  const valorParcela = total / numeroParcelas
 
   return { valorParcela, totalParcelas: numeroParcelas }
+}
+
+function obterJuros(numeroParcelas: number): number {
+  if (numeroParcelas >= 5 && numeroParcelas <= 8) return 0.05
+  return 0
 }
