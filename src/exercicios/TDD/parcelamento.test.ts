043bc1e1feb72c9b0fa0b3071811259e2d27f7e3
Júlia de Bona Jankus <juliabonaj@gmail.com>
2026-09-16 10:47:53 -0300

    chore: estrutura inicial do projeto de parcelamento (TDD)

---
+++ src/exercicios/TDD/parcelamento.test.ts
@@ -0,0 +1,4 @@
+import { calcularParcelamento } from './parcelamento'
+
+describe('calcularParcelamento', () => {
+})
---
+++ src/exercicios/TDD/parcelamento.ts
@@ -0,0 +1,11 @@
+export type ResultadoParcelamento = {
+  valorParcela: number
+  totalParcelas: number
+}
+
+export function calcularParcelamento(
+  valorCompra: number,
+  numeroParcelas: number,
+): ResultadoParcelamento {
+  throw new Error('não implementado')
+}
